d8d22f4d06f969e5d4b826a2bb5f2d6ff11f02b4
jojotenya <jojotenya@gmail.com>
2015-07-19 23:55:16 +0800

    My Django Girls app, fisrt commit

---
+++ mysite/local_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite',
+        'USER': 'root',
+        'PASSWORD': 'oblivn0374',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DEBUG = True
